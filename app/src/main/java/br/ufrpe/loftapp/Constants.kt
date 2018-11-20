979da0d64317dbbe6c7deed38ed12526091f8f67
Charles Silva <charlesdeoliveira95@gmail.com>
2018-11-20 17:13:40 -0300

    fixing master

--- app/src/main/java/br/ufrpe/loftapp/Constants.kt
+++
@@ -1,9 +0,0 @@
-package br.ufrpe.loftapp
-
-class Constants {
-    companion object {
-        val RequestCodeComidas=1010
-        var card  = ArrayList<Item>()
-        val RequestCodeDelete=1011
-    }
-}
--- app/src/main/java/br/ufrpe/loftapp/Item.kt
+++
@@ -1,7 +0,0 @@
-package br.ufrpe.loftapp
-
-import java.io.Serializable
-
-class Item(var name: String, var units: Int, var price: Double, var ingredients: String): Serializable {
-
-}
