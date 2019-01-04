d972d9e93a07f87e9f6ddc45c4a0f82184bacf1d
Charles Bronson de Oliveira Silva <charlesbronson_silva@apple.com>
2019-01-04 03:18:38 -0300

    Autenticação backend de Criar Conta e Login. Autenticação frontend de campos vazios e campo de email para Criar Conta.

--- app/src/main/java/br/ufrpe/loftapp2/credentials/CreateAccount.kt
+++ app/src/main/java/br/ufrpe/loftapp2/credentials/CreateAccount.kt
@@ -4,15 +4,25 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import br.ufrpe.loftapp2.MainActivity
 import br.ufrpe.loftapp2.R
 import kotlinx.android.synthetic.main.activity_create_account.*
+import com.google.firebase.auth.FirebaseAuth
 
 class CreateAccount : AppCompatActivity() {
+
+    // [START declare_auth]
+    private lateinit var mAuth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_account)
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance()
+        create_account_button.visibility = View.VISIBLE
+        createAccountProgress.visibility = View.INVISIBLE
 
         if(savedInstanceState != null){
             var email = savedInstanceState.getString("email")
@@ -34,8 +44,58 @@ class CreateAccount : AppCompatActivity() {
     }
 
     fun bttnClick(v: View){
-        val intent = Intent(this, Login::class.java )
-        startActivity(intent)
+
+        var email: String? = insert_email_edit.text.toString()
+        var name: String? = insert_name_edit.text.toString()
+        var phone: String? = insert_phone_edit.text.toString()
+        var password: String? = insert_password_edit.text.toString()
+
+        var invalidInfo: String = getString(R.string.invalidInformation)
+        var invalidEmail: String = getString(R.string.invalidEmail)
+
+        if (email!!.isEmpty() || name!!.isEmpty() || phone!!.isEmpty() || password!!.isEmpty() || !isValidEmail(email)){
+            if (!isValidEmail(email)){
+                showMessage(invalidEmail)
+            }
+            else{
+                showMessage(invalidInfo)
+            }
+        }
+        else{
+            createAccount (name, phone, email, password)
+        }
+    }
+
+    private fun createAccount(name: String, phone: String, email: String, password: String) {
+
+        var accountCreated: String = getString(R.string.accountCreatedSuccessfuly)
+        create_account_button.visibility = View.INVISIBLE
+        createAccountProgress.visibility = View.VISIBLE
+
+        mAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task ->
+                if (task.isSuccessful) {
+                    showMessage(accountCreated)
+                    val intent = Intent(this, Login::class.java )
+                    startActivity(intent)
+                    finish()
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    create_account_button.visibility = View.VISIBLE
+                    createAccountProgress.visibility = View.INVISIBLE
+                    showMessage(task.exception!!.message.toString())
+                }
+            }
+
+    }
+
+    private fun isValidEmail(target: CharSequence): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches()
+    }
+
+    private fun showMessage(string: String){
+        Toast.makeText(applicationContext, string, Toast.LENGTH_LONG).show()
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
--- app/src/main/java/br/ufrpe/loftapp2/credentials/Login.kt
+++ app/src/main/java/br/ufrpe/loftapp2/credentials/Login.kt
@@ -4,15 +4,26 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import br.ufrpe.loftapp2.MainActivity
 import br.ufrpe.loftapp2.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class Login : AppCompatActivity() {
 
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        signInButton.visibility = View.VISIBLE
+        login_progress.visibility = View.INVISIBLE
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance()
 
         if(savedInstanceState != null){
             var email = savedInstanceState.getString("emailField")
@@ -23,6 +34,8 @@ class Login : AppCompatActivity() {
     }
 
     override fun onResume() {
+        signInButton.visibility = View.VISIBLE
+        login_progress.visibility = View.INVISIBLE
         super.onResume()
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         actionBar?.hide()
@@ -34,9 +47,43 @@ class Login : AppCompatActivity() {
     }
 
     fun bttnClick(v: View){
-        val intent = Intent(this, MainActivity::class.java )
-        startActivity(intent)
+
+        var email: String? = emailField.text.toString()
+        var password: String? = passwordField.text.toString()
+
+        var message:String = getString(R.string.infoMissing)
+
+        if (email!!.isEmpty() || password!!.isEmpty()){
+            showMessage(message)
+        }
+        else{
+            login(emailField.text.toString(), passwordField.text.toString())
+        }
     }
+
+    private fun login(email: String, password: String) {
+        signInButton.visibility = View.INVISIBLE
+        login_progress.visibility = View.VISIBLE
+        var failMessage: String = getString(R.string.invalidUser)
+
+        mAuth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener { task ->
+                if(task.isSuccessful) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }else{
+                    signInButton.visibility = View.VISIBLE
+                    login_progress.visibility = View.INVISIBLE
+                    showMessage(failMessage)
+                }
+            }
+
+    }
+
+    private fun showMessage(string: String) {
+        Toast.makeText(applicationContext, string, Toast.LENGTH_LONG).show()
+    }
+
 
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
