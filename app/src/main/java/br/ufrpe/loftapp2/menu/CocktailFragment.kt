278c750fd50c9083f14acb705d28701127733e0f
Allan Moura <allan.moura16@gmail.com>
2019-01-06 22:26:09 -0200

    added data download from firebase through all fragments

--- app/src/main/java/br/ufrpe/loftapp2/menu/CocktailFragment.kt
+++ app/src/main/java/br/ufrpe/loftapp2/menu/CocktailFragment.kt
@@ -8,29 +8,43 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import br.ufrpe.loftapp2.R
 import br.ufrpe.loftapp2.adapter.ItemAdapter
 import br.ufrpe.loftapp2.model.Item
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.food_layout.view.*
+import java.util.HashMap
 
 class CocktailFragment: Fragment(){
 
     val itemList:ArrayList<Item> = ArrayList()
+    var listAdapter: ItemAdapter? = null
+
+    var mAuth: FirebaseAuth? = null
+    var firebaseDatabase: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        loadCocktailItem()
+        mAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        listAdapter = ItemAdapter(itemList, requireContext(), 1)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var v = LayoutInflater.from(container?.context).inflate(R.layout.cocktail_layout, container, false)
+        val v = LayoutInflater.from(container?.context).inflate(R.layout.cocktail_layout, container, false)
         v.rvItemList.addItemDecoration(SpaceItemDecoration(30, 50, 30))
         v.rvItemList.layoutManager = GridLayoutManager(context, 2)
-        v.rvItemList.adapter = ItemAdapter(itemList, requireContext(), 1)
+        v.rvItemList.adapter = listAdapter
+        loadCocktailsFromFirebase()
         return v
     }
-
+    /*
     private fun loadCocktailItem(){
         itemList.add(Item("1","Sex on The Beach", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
         itemList.add(Item("2","Margarita", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
@@ -56,6 +70,63 @@ class CocktailFragment: Fragment(){
         itemList.add(Item("18","Apple Cardamom Toddy", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
         itemList.add(Item("19","Hot Honey Butter", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
         itemList.add(Item("20","Ginger Apple Cooler", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
+
+        this.saveItem("Sex on The Beach", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/sexOnTheBeach.jpg?alt=media&token=16d48041-312c-44ed-82bd-685af2202a13")
+        this.saveItem("Margarita", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/margarita.jpg?alt=media&token=6e50f1aa-294f-4322-9bbf-0d051f1e5261")
+        this.saveItem("White Russian", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/whiteRussian.jpg?alt=media&token=9af57baa-1c5f-4395-a0df-82864624ef4a")
+        this.saveItem("Lemon Drop", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/lemonDrop.jpg?alt=media&token=244d2a6f-f722-491a-83cd-43f6a8731bc6")
+
+        this.saveItem("Gimlet", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Negroni", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Manhattan", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Mimosa", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+
+        this.saveItem("Sazerac", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Sidecar", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Martini", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Daiquiri", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+
+        this.saveItem("Tuthilltown Toddy", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Cinnamon Tequila Hot Toddy", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Prince Pum King", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+        this.saveItem("Docteur du Miel", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/cocktail.jpg?alt=media&token=6d369351-f04c-48d3-9eef-99a88f14b623")
+
+
+    }
+    */
+    private fun loadCocktailsFromFirebase(){
+        val cocktailRef = firebaseDatabase!!.getReference("cocktails")
+
+        cocktailRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                itemList.clear()
+
+                for(snapshot in p0.children){
+                    val hashMap = snapshot.value as HashMap<String, String>
+                    if(hashMap.size > 0){
+                        val item = Item( hashMap["id"].toString(), hashMap["name"].toString(), hashMap["units"].toString().toInt(),  hashMap["price"].toString().toDouble(), hashMap["ingredients"].toString() , hashMap["imageLink"].toString())
+                        itemList.add(item)
+                    }
+                }
+
+                listAdapter!!.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(context, "Problem reading from server", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    private fun saveItem(name:String, units: Int, price: Double, ingredients:String, imageLink: String  ){
+        val ref = firebaseDatabase!!.getReference("cocktails")
+        val cocktailId:String = ref.push().key!!
+        val cocktailItem = Item(cocktailId, name, units, price, ingredients, imageLink)
+
+        ref.child(cocktailId).setValue(cocktailItem).addOnCompleteListener {
+            Toast.makeText(context, "cocktail salva com sucesso", Toast.LENGTH_LONG ).show()
+        }
     }
 
     inner class SpaceItemDecoration(val spaceLeft: Int, val spaceTop: Int, val spaceRight: Int ): RecyclerView.ItemDecoration(){
--- app/src/main/java/br/ufrpe/loftapp2/menu/DrinkFragment.kt
+++ app/src/main/java/br/ufrpe/loftapp2/menu/DrinkFragment.kt
@@ -8,30 +8,45 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import br.ufrpe.loftapp2.R
 import br.ufrpe.loftapp2.adapter.ItemAdapter
 import br.ufrpe.loftapp2.model.Item
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.food_layout.view.*
+import java.util.HashMap
 
 class DrinkFragment: Fragment(){
 
     val itemList:ArrayList<Item> = ArrayList()
+    var listAdapter: ItemAdapter? = null
+
+    var mAuth: FirebaseAuth? = null
+    var firebaseDatabase: FirebaseDatabase? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        loadDrinkItem()
+        mAuth = FirebaseAuth.getInstance()
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        listAdapter = ItemAdapter(itemList, requireContext(), 1)
+
     }
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        var v = LayoutInflater.from(container?.context).inflate(R.layout.drink_layout, container, false)
+        val v = LayoutInflater.from(container?.context).inflate(R.layout.drink_layout, container, false)
         v.rvItemList.addItemDecoration(SpaceItemDecoration(30, 50, 30))
-        v.rvItemList.layoutManager = GridLayoutManager(context, 2)
-        v.rvItemList.adapter = ItemAdapter(itemList, requireContext(), 1)
+        v.rvItemList.layoutManager = GridLayoutManager( context, 2)
+        v.rvItemList.adapter = listAdapter
+        loadDrinkFromFirebase()
         return v
     }
-
+    /*
     private fun loadDrinkItem(){
         itemList.add(Item("1","Jack Daniel's", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
         itemList.add(Item("2","Red Label", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
@@ -57,8 +72,71 @@ class DrinkFragment: Fragment(){
         itemList.add(Item("18","Freixenet", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
         itemList.add(Item("19","Canevari", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
         itemList.add(Item("20","Bellini", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", ""))
-    }
 
+        /*
+        this.saveItem("Jack Daniel's", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/jackdeniels.jpg?alt=media&token=74b73bd8-b68d-4b4e-9558-42b8ba882172")
+        this.saveItem("Red Label", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/redLabel.jpg?alt=media&token=59b22476-5eea-4d7d-8597-fbb764c11408")
+        this.saveItem("Black Label", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/blackLabel.jpg?alt=media&token=142ff6fb-f98d-4843-9088-9d5891ba60a4")
+        this.saveItem("Black&White", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/blackWhite.jpg?alt=media&token=da8f1ce9-a86b-4953-8586-9bc7fb2ef516")
+
+        this.saveItem("Jose Cuervo", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("1800 Reposado", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Don Julio", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Patron", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+
+        this.saveItem("Blue Label", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Old Parr", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Chivas Regal", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Gold Label", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+
+        this.saveItem("Ciroc", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Absolut", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("AnestasiA", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Absolut Elyx", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+
+        this.saveItem("Chandon", 1, 5.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Freixenet", 1, 12.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Canevari", 1, 8.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        this.saveItem("Bellini 2000", 1, 10.0, "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum", "https://firebasestorage.googleapis.com/v0/b/loftapp-5ed42.appspot.com/o/bebida.jpg?alt=media&token=bfdd6b91-cdfe-4e5d-9de7-41b109047b71")
+        */
+
+    }
+*/
+    private fun loadDrinkFromFirebase(){
+        val drinkRef = firebaseDatabase!!.getReference("drinks")
+
+        drinkRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                itemList.clear()
+
+                for(snapshot in p0.children){
+                    val hashMap = snapshot.value as HashMap<String, String>
+                    if(hashMap.size > 0){
+                        val item = Item( hashMap["id"].toString(), hashMap["name"].toString(), hashMap["units"].toString().toInt(),  hashMap["price"].toString().toDouble(), hashMap["ingredients"].toString() , hashMap["imageLink"].toString())
+                        itemList.add(item)
+                    }
+                }
+
+                listAdapter!!.notifyDataSetChanged()
+
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                Toast.makeText(context, "Problem reading from server", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+    /*
+    private fun saveItem(name:String, units: Int, price: Double, ingredients:String, imageLink: String  ){
+        val ref = firebaseDatabase!!.getReference("drinks")
+        val drinkId:String = ref.push().key!!
+        val drinkItem = Item(drinkId, name, units, price, ingredients, imageLink)
+
+        ref.child(drinkId).setValue(drinkItem).addOnCompleteListener {
+            Toast.makeText(context, "drink salva com sucesso", Toast.LENGTH_LONG ).show()
+        }
+    }
+*/
     inner class SpaceItemDecoration(val spaceLeft: Int, val spaceTop: Int, val spaceRight: Int ): RecyclerView.ItemDecoration(){
         override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
             super.getItemOffsets(outRect, view, parent, state)
